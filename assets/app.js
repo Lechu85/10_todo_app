ba3b4d6a957d149382a7369540abaa2985be59af
Lechu85 <leszek.chopcian@gmail.com>
2022-11-30 17:53:32 +0100

    First commit on new computer

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+
 import 'bootstrap';
 
-//import { Tooltip, Toast, Popover } from 'bootstrap'
-
 //const getNiceMessage = require('./js/components/get_nice_message')
-
 import getNiceMessage from './js/components/get_nice_message';
 
-//import $ from 'jquery' (package name)
 //jeżeli nie ma ./ albo ../ to jest moduł, inaczej szuka relatywnioe to tego pliku
-//global.$ = $;
-//only for legacy code to jquery works
-
 
 
 
--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,20 +1,21 @@
 import '../styles/tasks.scss';
 //nie jestem pewien czy jest potrzebne
-
 /* document.querySelector('.confirm-delete').addEventListener('click', event => {
     return confirm('Are you sure to delete?');
 }); */
 
+//zmienną deklarujemy jako window.variableName
+
 // info ukrywamy pole checkbox +Opis dla zaawansowanej wyszukiwarki
-
-const myCollapsible = document.getElementById('collapseAdvancedSearch')
-
 // question - czy tutaj zasaday SOLID zostałą złamana? jedna funkcja chowa i ukrywa? Zrobić dwie funkcje?
 function toggleAddDescriptionBtn(hideOrShow){
-    btn_wrapper = document.getElementById('addDescriptionWrapper')
+
+    const btn_wrapper = document.getElementById('addDescriptionWrapper')
     btn_wrapper.style.display = hideOrShow
     btn_wrapper.querySelector('input[type="checkbox"]').checked = false
 }
+
+const myCollapsible = document.getElementById('collapseAdvancedSearch')
 
 myCollapsible.addEventListener('hide.bs.collapse', event => {
     toggleAddDescriptionBtn('flex')
@@ -25,7 +26,7 @@ myCollapsible.addEventListener('show.bs.collapse', event => {
 })
 
 
-// info wysylamy grupowe akcje
+// info wysylamy grupowe akcje z liosty zadań
 function doGroupAction(event, type_action) {
 
     const task_checkboxes = document.querySelectorAll('input[name="task[]"]:checked')
@@ -33,16 +34,14 @@ function doGroupAction(event, type_action) {
     if (task_checkboxes.length === 0) {
         alert('Zaznacz przynajmniej jedno zadanie')
         event.preventDefault()
-
     } else {
         document.getElementById('group_action_name').value = type_action
     }
-
 }
 
 document.getElementById('btn_group_action_delete').addEventListener('click', function(event) {
     doGroupAction(event, 'delete')
 });
-document.getElementById('btn_group_action_done').addEventListener('click', function(event) {
+document.getElementById('btn_group_action_done').addEventListener('click', event => {
     doGroupAction(event, 'done')
 });
